d296c18d6ce9734691904282aca70da9b3b847aa
Iuri <bokin@me.com>
2023-09-11 15:50:38 -0300

    Initial Commit

---
+++ Enkanto/EnkantoApp.swift
@@ -0,0 +1,17 @@
+//
+//  EnkantoApp.swift
+//  Enkanto
+//
+//  Created by Iuri Ferreira on 11/09/23.
+//
+
+import SwiftUI
+
+@main
+struct EnkantoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
