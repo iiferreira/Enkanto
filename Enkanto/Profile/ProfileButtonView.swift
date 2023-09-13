7881d1ecb641af3d942b6685b8a5156744d085bb
Iuri <bokin@me.com>
2023-09-13 13:48:28 -0300

    refactor : profile view

---
+++ Enkanto/Profile/ProfileButtonView.swift
@@ -0,0 +1,20 @@
+//
+//  ProfileButtonView.swift
+//  Enkanto
+//
+//  Created by Iuri Ferreira on 13/09/23.
+//
+
+import SwiftUI
+
+struct ProfileButtonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ProfileButtonView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileButtonView()
+    }
+}
---
+++ Enkanto/Profile/ProfileSwipePromo.swift
@@ -0,0 +1,20 @@
+//
+//  ProfileSwipePromo.swift
+//  Enkanto
+//
+//  Created by Iuri Ferreira on 13/09/23.
+//
+
+import SwiftUI
+
+struct ProfileSwipePromo: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ProfileSwipePromo_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileSwipePromo()
+    }
+}
