a0016eacb6e67324adc89a97a9041c1e8d566609
Iuri Ferreira <bokin@me.com>
2023-09-29 23:15:57 -0300

    done: Purchase Popup View

--- Enkanto/Popups/PurchasePopup.swift
+++ Enkanto/Popups/PurchasePopup.swift
@@ -11,6 +11,14 @@ struct PurchasePopup: View {
     
     var screen = UIScreen.main.bounds
     
+    @State private var selectedIndex : Int = 1
+    
+    var subscriptions : [Subscription] = [
+        Subscription.example1,
+        Subscription.example2,
+        Subscription.example3
+    ]
+    
     var body: some View {
         GeometryReader { geo in
             VStack {
@@ -26,6 +34,15 @@ struct PurchasePopup: View {
                     PurchasesSwipePromo()
                         .frame(height: geo.size.height / 3 )
                         .padding(.top, -35)
+                    
+                    HStack {
+                        ForEach(subscriptions.indices) { subIndex in
+                            PurchasesOptionsView(subscription: subscriptions[subIndex], isSelected: subIndex == selectedIndex)
+                                .onTapGesture {
+                                    selectedIndex = subIndex
+                                }
+                        }
+                    }
                         
                     
                     Text("3 Purchase Options")
